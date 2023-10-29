32cf42c73889ab0ac48ec163be6c7c30941d5ee6
Peter He <1036566848@qq.com>
2023-10-29 20:26:05 +0800

    2023年10月29日 环境配置

---
+++ src/app.config.js
@@ -0,0 +1,14 @@
+export default {
+  pages: [
+    "pages/index/index",
+    "pages/index/mall",
+    "pages/index/order",
+    "pages/index/mine",
+  ],
+  window: {
+    backgroundTextStyle: "light",
+    navigationBarBackgroundColor: "#fff",
+    navigationBarTitleText: "WeChat",
+    navigationBarTextStyle: "black",
+  },
+};
